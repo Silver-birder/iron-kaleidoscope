017032ce881131564af3ef14216edbc533324671
shibamoto <silverbirder@gmail.com>
2021-09-26 20:11:36 +0900

    ✍️ learn llvm fizzbuzz

--- chapters/iw/src/main.rs
+++ chapters/iw/src/main.rs
@@ -5,7 +5,9 @@ use inkwell::builder::Builder;
 use inkwell::context::Context;
 use inkwell::execution_engine::{ExecutionEngine};
 use inkwell::module::Module;
+use inkwell::IntPredicate::EQ;
 use std::error::Error;
+use std::ptr::null;
 
 struct CodeGen<'ctx> {
     context: &'ctx Context,
@@ -16,31 +18,27 @@ struct CodeGen<'ctx> {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let context = Context::create();
-    let module = context.create_module("main");
-    let builder = context.create_builder();
+    let module = context.create_module("fizzbuzz");
     let i64_type = context.i64_type();
-    let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
-    let function = module.add_function("sum", fn_type, None);
-    let basic_block = context.append_basic_block(function, "entry");
-    builder.position_at_end(basic_block); // module > function > block を定義させてから、buildを動かすんだ!
-
+    let f64_type = context.f64_type();
+    let fn_type = i64_type.fn_type(&[i64_type.into()], false);
+    let function = module.add_function("fizzbuzz", fn_type, None);
+    let block = context.append_basic_block(function, "entry");
+    let builder = context.create_builder();
+    builder.position_at_end(block);
     let x = function.get_nth_param(0).unwrap().into_int_value();
-    let y = function.get_nth_param(1).unwrap().into_int_value();
-    let z = function.get_nth_param(2).unwrap().into_int_value();
-    let sum = builder.build_int_add(x, y, "sum");
-    let sum = builder.build_int_add(z, sum, "sum");
-    builder.build_return(Some(&sum));
+    let x3 = builder.build_int_signed_rem(x, i64_type.const_int(3, false), "rem");
+    let x5 = builder.build_int_signed_rem(x, i64_type.const_int(5, false), "rem");
+    let x15 = builder.build_int_signed_rem(x, i64_type.const_int(15, false), "rem");
+    let cmp = builder.build_int_compare(EQ, x3, i64_type.const_int(0, false), "EQ");
+    builder.build_unsigned_int_to_float(cmp, f64_type, "cmp");
+    builder.build_return(Some(&x15));
 
     let e = module.create_jit_execution_engine(OptimizationLevel::None)?;
-
-    // 途中経過は、module.print_to_fileでllファイルを読むんだ! (RUST_BACKTRACE=1も大切だ)
-
-    unsafe { 
-        let x = 1u64;
-        let y = 2u64;
-        let z = 3u64;
-        let s = e.get_function::<unsafe extern "C" fn(u64, u64, u64)-> u64>("sum")?.call(x, y , z);
-        print!("{}", s);
-    };
+    unsafe {
+        let x = 3u64;
+        let r = e.get_function::<unsafe extern "C" fn(u64)-> u64>("fizzbuzz")?.call(x);
+        print!("{}", r);
+    }
     Ok(())
 }
