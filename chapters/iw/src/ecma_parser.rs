cadf2de4e8d693bc3dd67c4d748d9ae15da6a01e
shibamoto <silverbirder@gmail.com>
2021-09-30 18:56:21 +0900

    🙂 parse 1 + 1 with ecma parser

---
+++ chapters/iw/src/ecma_parser.rs
@@ -0,0 +1,79 @@
+#[macro_use]
+extern crate swc_common;
+extern crate swc_ecma_parser;
+extern crate swc_ecma_ast;
+
+use std::path::Path;
+
+use swc_common::sync::Lrc;
+use swc_common::{
+    errors::{ColorConfig, Handler},
+    FileName, FilePathMapping, SourceMap,
+};
+use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
+use swc_ecma_ast::{Lit, Number};
+use swc_ecma_ast::Lit::Num;
+
+fn main() {
+    let cm: Lrc<SourceMap> = Default::default();
+    let handler =
+        Handler::with_tty_emitter(ColorConfig::Auto, true, false,
+        Some(cm.clone()));
+
+    // Real usage
+    let fm = cm
+        .load_file(Path::new("./src/test.js"))
+        .expect("failed to load test.js");
+    // let fm = cm.new_source_file(
+    //     FileName::Custom("test.js".into()),
+    //     "function foo() {}".into(),
+    // );
+    let lexer = Lexer::new(
+        // We want to parse ecmascript
+        Syntax::Es(Default::default()),
+        // JscTarget defaults to es5
+        Default::default(),
+        StringInput::from(&*fm),
+        None,
+    );
+
+    let mut parser = Parser::new_from(lexer);
+
+    for e in parser.take_errors() {
+        e.into_diagnostic(&handler).emit();
+    }
+
+    let _module = parser
+        .parse_module()
+        .map_err(|mut e| {
+            // Unrecoverable fatal error occurred
+            e.into_diagnostic(&handler).emit()
+        })
+        .expect("failed to parser module");
+
+
+    for b in _module.body {
+        let stmt = b.stmt().unwrap();
+        let exprStmt = stmt.expr().unwrap();
+        let binExpr = exprStmt.expr.bin().unwrap();
+        let left_lit = binExpr.left.lit().unwrap();
+        let right_lit = binExpr.right.lit().unwrap();
+        let left_value = match left_lit {
+            Num(n) => {
+                n.value
+            },
+            _ => {
+                0f64
+            }
+        };
+        let right_value = match right_lit {
+            Num(n) => {
+                n.value
+            },
+            _ => {
+                0f64
+            }
+        };
+        println!("{:?}", left_value + right_value);
+    }
+}
